55db14fcb8253cfbc26fcf8666fe759486c36c80
Himanshu Nahata <himanshunahata@gmail.com>
2023-05-04 00:11:20 +0000

    Added SAP Sources and creating staging models for each source

---
+++ models/C_STAGING/SAP/stg_SAP_CONTACT.sql
@@ -0,0 +1,28 @@
+with source as (
+
+    select * from {{ source('SAP_HANA', 'CONTACT') }}
+
+),
+
+renamed as (
+
+    select
+        source_system,
+        customer_number,
+        person_number,
+        title,
+        name_first,
+        name_last,
+        name_text,
+        pers_type,
+        default_comm,
+        tel_number,
+        fax_number,
+        email,
+        update_time
+
+    from source
+
+)
+
+select * from renamed
